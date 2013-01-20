00b647f3f29b58e545c6fe05ba9a38e3b6993e37
Michael P. Geraci <me@mgeraci.com>
2013-01-20 13:25:34 -0500

    add disabled state for form

--- javascript/main.js
+++ javascript/main.js
@@ -29,7 +29,7 @@ loadInitials = function () {
   var initialsImage = "images/initials-banner.png"
   initialsCB = function () {
     $("header").append("<img src='"+initialsImage+"'>");
-    $("header img").animate({top: 8, opacity: 1}, 500, function () {
+    $("header img").animate({top: 8, opacity: 1}, 800, function () {
       $(".column").fadeIn(function () {
         $("#map").append(map);
         $("body").append('<div class="footer" />');
